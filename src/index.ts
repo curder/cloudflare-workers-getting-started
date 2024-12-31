c7d542c7263b5ffa4d579e05381ad17ea7f1c7e7
curder <q.curder@gmail.com>
2024-12-31 11:11:18 +0800

    Test auto deployment

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from 'hono';
 
 const app = new Hono();
-app.get('/', c => c.json({ key: 'Hello world' }));
+app.get('/', c => c.json({ key: 'Hello world!' }));
 
 export default app;
